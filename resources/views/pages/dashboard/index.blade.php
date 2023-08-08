aa43943b84fdb94cf20965a8c9280d6415825e97
Leonardo Lopes <leonardolivelopes2@gmail.com>
2023-08-08 18:29:08 -0300

    copy link button

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -78,20 +78,46 @@
                         <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
                             Clique para copiar o link e enviar no WhatsApp para seus camaradas se ajuntarem ao pelotão
                         </p>
-                        <div class="mt-10 flex items-center justify-center gap-x-6">
-                            <button type="button" x-on:click="copyToClipboard"
+                        <div x-data="{
+                            copyText: '{{ $shareURL }}',
+                            copyNotification: false,
+                            copyToClipboard() {
+                                navigator.clipboard.writeText(this.copyText);
+                                this.copyNotification = true;
+                                let that = this;
+                                setTimeout(function() {
+                                    that.copyNotification = false;
+                                }, 3000);
+                            }
+                        }" class="mt-10 flex items-center justify-center gap-x-6">
+                            <button type="button" @click="copyToClipboard();"
                                 class="flex items-center gap-2 rounded-md bg-green-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
+                                <div class="relative z-20 flex items-center">
+                                    <div x-show="copyNotification" x-transition:enter="transition ease-out duration-300"
+                                        x-transition:enter-start="opacity-0 translate-x-2"
+                                        x-transition:enter-end="opacity-100 translate-x-0"
+                                        x-transition:leave="transition ease-in duration-300"
+                                        x-transition:leave-start="opacity-100 translate-x-0"
+                                        x-transition:leave-end="opacity-0 translate-x-2" class="absolute left-0"
+                                        x-cloak>
+                                        <div
+                                            class="px-3 h-7 -ml-1.5 items-center flex text-xs bg-green-500 border-r border-green-500 -translate-x-full text-white rounded">
+                                            <span>Copiado!</span>
+                                            <div
+                                                class="absolute right-0 inline-block h-full -mt-px overflow-hidden translate-x-3 -translate-y-2 top-1/2">
+                                                <div
+                                                    class="w-3 h-3 origin-top-left transform rotate-45 bg-green-500 border border-transparent">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="flex items-center justify-center h-8 text-xs group">
+                                        <x-icon name="copy" x-show="!copyNotification" class="w-4 h-4 stroke-current text-white" />
+                                        <x-icon name="copied" x-show="copyNotification" class="w-4 h-4 stroke-current text-white" />
+                                    </div>
+                                </div>
                                 Clique aqui para copiar
-                                <span>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        class="h-4 w-4"
-                                        fill="currentColor"
-                                        viewBox="0 0 24 24">
-                                        <path
-                                        d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
-                                    </svg>
-                                </span>
+                                <x-icon style="social" name="whatsapp" />
                             </button>
                         </div>
                         <svg viewBox="0 0 1024 1024"
@@ -137,14 +163,4 @@
             <!-- More items... -->
         </ul>
     </aside>
-
-    <textarea id="share-button">{{ $shareURL }}</textarea>
-    @push('scripts')
-        <script>
-            function copyToClipboard() {
-                document.getElementById("share-button").select();
-                document.execCommand('copy');
-            }
-        </script>
-    @endpush
 </x-app-layout>
